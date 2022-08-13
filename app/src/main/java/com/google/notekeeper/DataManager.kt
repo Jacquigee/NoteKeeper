a499ee054bf89d781cea7bed8508a19bd6ec4da7
Jcquigee <gitaujaquiline@gmail.com>
2022-08-14 02:07:16 +0300

    Adding Data files

---
+++ app/src/main/java/com/google/notekeeper/DataManager.kt
@@ -0,0 +1,21 @@
+package com.google.notekeeper
+
+class DataManager {
+    val courses = HashMap<String, CourseInfo>()
+    val notes = ArrayList<NoteInfo>()
+
+    init {
+        initializeCourses()
+    }
+
+    private fun initializeCourses(){
+        var course = CourseInfo("android_intents", "Android Programming with Intents")
+        courses[course.courseId] = course
+        course = CourseInfo(courseId = "android_async", title = "Android Async Programming and Services")
+        courses[course.courseId] = course
+        course = CourseInfo(title ="Java Fundamentals: The Java Language", courseId =  "java_lang")
+        courses[course.courseId] = course
+        course = CourseInfo("java_core", "Java Fundamentals: The core Platform")
+        courses[course.courseId] = course
+    }
+}
---
+++ app/src/main/java/com/google/notekeeper/NoteKeeperData.kt
@@ -0,0 +1,5 @@
+package com.google.notekeeper
+
+class CourseInfo (val courseId: String, val title: String)
+
+class NoteInfo (var course: CourseInfo, var title: String, var text: String)
